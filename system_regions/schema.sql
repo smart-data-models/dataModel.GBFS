c77b959c8954f7879aa10f25ad78f4ce25013d5a
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:55:59 +0200

    test of SQL export

--- system_regions/schema.sql
+++ system_regions/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model system_regions of the subject dataModel.GBFS for a postgreSQL database. Pending translation of enumerations and multityped attributes */
-CREATE TYPE system_regions_type AS ENUM ('system_regions');CREATE TYPE version_type AS ENUM ('1.1-RC', 1.1, '2.0-RC', 2.0, '2.1-RC', '2.1-RC2', 2.1, 2.2, '3.0-RC', 3.0);
-CREATE TABLE system_regions (data json, id text, last_updated integer, ttl integer, type system_regions_type, version version_type);
+/* (Beta) Export of data model system_regions of the subject dataModel.GBFS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE system_regions_type AS ENUM ('system_regions');CREATE TYPE version_type AS ENUM ('1.1-RC','1.1','2.0-RC','2.0','2.1-RC','2.1-RC2','2.1','2.2','3.0-RC','3.0');
+CREATE TABLE system_regions (data JSON, last_updated INTEGER, ttl INTEGER, type system_regions_type, version version_type);
