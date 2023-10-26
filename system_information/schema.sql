6eb7bbf388b85a0748822d4afd0e5fc16a3f0e8c
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:06:42 +0200

    test of SQL export

--- system_information/schema.sql
+++ system_information/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model system_information of the subject dataModel.GBFS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE system_information_type AS ENUM ('system_information');CREATE TYPE version_type AS ENUM ('1.1-RC','1.1','2.0','2.1-RC','2.1-RC2','2.1','2.2','3.0');
-CREATE TABLE system_information (data JSON, last_updated INTEGER, ttl INTEGER, type system_information_type, version version_type);
+CREATE TABLE system_information (data JSON, id TEXT PRIMARY KEY, last_updated INTEGER, ttl INTEGER, type system_information_type, version version_type);
