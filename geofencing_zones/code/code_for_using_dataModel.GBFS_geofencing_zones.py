c240a6655f088339ae907f498849ab25d6ad1aac
Alberto Abella <alberto.abella@fiware.org>
2024-05-14 19:02:20 +0200

    created example of code

---
+++ geofencing_zones/code/code_for_using_dataModel.GBFS_geofencing_zones.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "geofencing_zones"
+subject = "dataModel.GBFS"
+last_updated = {'type': 'Property', 'value': 1604198100}
+attribute = "last_updated"
+value = last_updated
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+ttl = {'type': 'Property', 'value': 60}
+attribute = "ttl"
+value = ttl
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+version = "{'type': 'Property', 'value': '3.0'}"
+attribute = "version"
+value = version
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+data = {'type': 'Property', 'value': {'geofencing_zones': {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'geometry': {'type': 'MultiPolygon', 'coordinates': [[[[-122.578067, 45.562982], [-122.661838, 45.562741], [-122.661151, 45.504542], [-122.578926, 45.5046625], [-122.578067, 45.562982]]], [[[-122.65068, 45.548197], [-122.650852, 45.534731], [-122.630939, 45.535212], [-122.630424, 45.548197], [-122.65068, 45.548197]]]]}, 'properties': {'name': 'NE 24th/NE Knott', 'start': 1593878400, 'end': 1593907260, 'rules': [{'vehicle_type_id': ['moped1', 'car1'], 'ride_allowed': False, 'ride_through_allowed': True, 'maximum_speed_kph': 10}]}}]}}}
+attribute = "data"
+value = data
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
