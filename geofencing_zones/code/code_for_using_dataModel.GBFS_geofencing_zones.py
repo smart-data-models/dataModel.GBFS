2560e69e546dd268206ca9bc24b743b7b5fba69f
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:31:49 +0200

    created example of code

--- geofencing_zones/code/code_for_using_dataModel.GBFS_geofencing_zones.py
+++ geofencing_zones/code/code_for_using_dataModel.GBFS_geofencing_zones.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "geofencing_zones"
 subject = "dataModel.GBFS"
-last_updated = {'type': 'Property', 'value': 1604198100}
+last_updated = 1604198100
 attribute = "last_updated"
 value = last_updated
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-ttl = {'type': 'Property', 'value': 60}
+ttl = 60
 attribute = "ttl"
 value = ttl
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-version = "{'type': 'Property', 'value': '3.0'}"
+version = "3.0"
 attribute = "version"
 value = version
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-data = {'type': 'Property', 'value': {'geofencing_zones': {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'geometry': {'type': 'MultiPolygon', 'coordinates': [[[[-122.578067, 45.562982], [-122.661838, 45.562741], [-122.661151, 45.504542], [-122.578926, 45.5046625], [-122.578067, 45.562982]]], [[[-122.65068, 45.548197], [-122.650852, 45.534731], [-122.630939, 45.535212], [-122.630424, 45.548197], [-122.65068, 45.548197]]]]}, 'properties': {'name': 'NE 24th/NE Knott', 'start': 1593878400, 'end': 1593907260, 'rules': [{'vehicle_type_id': ['moped1', 'car1'], 'ride_allowed': False, 'ride_through_allowed': True, 'maximum_speed_kph': 10}]}}]}}}
+data = {'geofencing_zones': {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'geometry': {'type': 'MultiPolygon', 'coordinates': [[[[-122.578067, 45.562982], [-122.661838, 45.562741], [-122.661151, 45.504542], [-122.578926, 45.5046625], [-122.578067, 45.562982]]], [[[-122.65068, 45.548197], [-122.650852, 45.534731], [-122.630939, 45.535212], [-122.630424, 45.548197], [-122.65068, 45.548197]]]]}, 'properties': {'name': 'NE 24th/NE Knott', 'start': 1593878400, 'end': 1593907260, 'rules': [{'vehicle_type_id': ['moped1', 'car1'], 'ride_allowed': False, 'ride_through_allowed': True, 'maximum_speed_kph': 10}]}}]}}
 attribute = "data"
 value = data
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
