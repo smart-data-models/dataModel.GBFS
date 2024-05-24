9a172dd37f09e5a47f7f4619d45ea6d8677741e0
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:32:21 +0200

    created example of code

--- vehicle_types/code/code_for_using_dataModel.GBFS_vehicle_types.py
+++ vehicle_types/code/code_for_using_dataModel.GBFS_vehicle_types.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "vehicle_types"
 subject = "dataModel.GBFS"
-last_updated = {'type': 'Property', 'value': 1609866247}
+last_updated = 1609866247
 attribute = "last_updated"
 value = last_updated
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-ttl = {'type': 'Property', 'value': 0}
+ttl = 0
 attribute = "ttl"
 value = ttl
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-version = "{'type': 'Property', 'value': '3.0'}"
+version = "3.0"
 attribute = "version"
 value = version
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-data = {'type': 'Property', 'value': {'vehicle_types': [{'vehicle_type_id': 'abc123', 'form_factor': 'bicycle', 'propulsion_type': 'human', 'name': 'Example Basic Bike', 'default_reserve_time': 30, 'return_type': ['any_station', 'free_floating'], 'vehicle_assets': {'icon_url': 'https://www.example.com/assets/icon_bicycle.svg', 'icon_url_dark': 'https://www.example.com/assets/icon_bicycle_dark.svg', 'icon_last_modified': '2021-06-15'}, 'default_pricing_plan_id': 'bike_plan_1', 'pricing_plan_ids': ['bike_plan_1', 'bike_plan_2', 'bike_plan_3']}, {'vehicle_type_id': 'def456', 'form_factor': 'scooter', 'propulsion_type': 'electric', 'name': 'Example E-scooter V2', 'default_reserve_time': 30, 'max_range_meters': 12345, 'return_type': ['free_floating'], 'vehicle_assets': {'icon_url': 'https://www.example.com/assets/icon_escooter.svg', 'icon_url_dark': 'https://www.example.com/assets/icon_escooter_dark.svg', 'icon_last_modified': '2021-06-15'}, 'default_pricing_plan_id': 'scooter_plan_1'}, {'vehicle_type_id': 'car1', 'form_factor': 'car', 'propulsion_type': 'combustion', 'name': 'Four-door Sedan', 'default_reserve_time': 0, 'max_range_meters': 523992, 'return_type': ['roundtrip_station'], 'vehicle_assets': {'icon_url': 'https://www.example.com/assets/icon_car.svg', 'icon_url_dark': 'https://www.example.com/assets/icon_car_dark.svg', 'icon_last_modified': '2021-06-15'}, 'default_pricing_plan_id': 'car_plan_1'}]}}
+data = {'vehicle_types': [{'vehicle_type_id': 'abc123', 'form_factor': 'bicycle', 'propulsion_type': 'human', 'name': 'Example Basic Bike', 'default_reserve_time': 30, 'return_type': ['any_station', 'free_floating'], 'vehicle_assets': {'icon_url': 'https://www.example.com/assets/icon_bicycle.svg', 'icon_url_dark': 'https://www.example.com/assets/icon_bicycle_dark.svg', 'icon_last_modified': '2021-06-15'}, 'default_pricing_plan_id': 'bike_plan_1', 'pricing_plan_ids': ['bike_plan_1', 'bike_plan_2', 'bike_plan_3']}, {'vehicle_type_id': 'def456', 'form_factor': 'scooter', 'propulsion_type': 'electric', 'name': 'Example E-scooter V2', 'default_reserve_time': 30, 'max_range_meters': 12345, 'return_type': ['free_floating'], 'vehicle_assets': {'icon_url': 'https://www.example.com/assets/icon_escooter.svg', 'icon_url_dark': 'https://www.example.com/assets/icon_escooter_dark.svg', 'icon_last_modified': '2021-06-15'}, 'default_pricing_plan_id': 'scooter_plan_1'}, {'vehicle_type_id': 'car1', 'form_factor': 'car', 'propulsion_type': 'combustion', 'name': 'Four-door Sedan', 'default_reserve_time': 0, 'max_range_meters': 523992, 'return_type': ['roundtrip_station'], 'vehicle_assets': {'icon_url': 'https://www.example.com/assets/icon_car.svg', 'icon_url_dark': 'https://www.example.com/assets/icon_car_dark.svg', 'icon_last_modified': '2021-06-15'}, 'default_pricing_plan_id': 'car_plan_1'}]}
 attribute = "data"
 value = data
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
